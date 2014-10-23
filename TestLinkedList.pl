d8da2c51ccb8265611481ced9b4b3bacc3e382d3
Hu Yin <nickhodge8@Hotmail.com>
2014-10-23 17:08:39 -0500

    Create TestLinkedList.pl

---
+++ TestLinkedList.pl
@@ -0,0 +1,25 @@
+use LinkedList;
+use 5.12.0;
+
+my $list = LinkedList->new();
+
+$list->add_node(1);
+
+say $list->get_head()->get_val();
+
+$list->add_node(2);
+$list->add_node(3);
+$list->add_node(4);
+$list->add_node(5);
+
+my $node = $list->get_head()->get_next();
+
+say $node->get_val();
+
+$list->delete_node(3);
+
+my $next = $node->get_next();
+
+say $next->get_val();
+
+say $list->get_size();
